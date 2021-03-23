702f7b51f5eeaaaf2f9cb5addf33e649fbe1a6b4
hottim900 <hottim900@gmail.com>
2021-03-23 17:57:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# hottim900.cs09g
+## NYCU_Software_Testing_2021_IOC_Lab_3
+
+
+
+- Pass
+
+![](img/pass_board.png)
+
+![](img/pass_info.png)
+
+- Fail
+
+![](img/fail_board.png)
+
+![](img/fail_info.png)
---
+++ src/test/java/PriorityQueueTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class PriorityQueueTest{
+    public static Stream<Arguments> streamParameters() {
+        return Stream.of(
+                arguments(new int[]{1, 3, 4, 2}, new int[]{1, 2, 3, 4}),
+                arguments(new int[]{7, 9, 2, 5}, new int[]{2, 5, 7, 9}),
+                arguments(new int[]{5, 8, 0, 2}, new int[]{0, 2, 5, 8}),
+                arguments(new int[]{6, 4, 8, 1}, new int[]{1, 4, 6, 8}),
+                arguments(new int[]{0, 5, 7, 9}, new int[]{0, 5, 7, 9})
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("streamParameters")
+    public void parameterizedTest(int[] random_array, int[] correct_array) {
+        PriorityQueue pqTester = new PriorityQueue();
+        for (int e : random_array) {
+            // offer: Inserts the specified element into this priority queue.
+            pqTester.offer(e+1);
+        }
+        for (int e : correct_array) {
+            // poll: Retrieves and removes the head of this queue, or returns null if this queue is empty.
+            assertEquals(e, pqTester.poll());
+        }
+    }
+    @Test
+    public void initialCapacityTest() {
+        // Throws: IllegalArgumentException - if initialCapacity is less than 1
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PriorityQueue(0);
+        });
+    }
+
+    @Test
+    public void offerTest() {
+        // Throws: NullPointerException - if the specified element is null
+        assertThrows(NullPointerException.class, () -> new PriorityQueue().offer(null));
+    }
+
+    @Test
+    public void addTest() {
+        // Throws: NullPointerException - if the specified element is null
+        assertThrows(NullPointerException.class, () -> new PriorityQueue().add(null));
+    }
+}
